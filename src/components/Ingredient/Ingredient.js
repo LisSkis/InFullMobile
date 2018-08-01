be3d33f31f049d6c1fa2cf7b380768e4fd68648e
Marcin <marcin@appstract.software>
2018-08-01 13:42:06 +0200

    fix

--- src/components/Ingredient/Ingredient.js
+++ src/components/Ingredient/Ingredient.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-const Ingredient = ({ item }) => (
-   <div className="recipe-ingredient">
-      {item.name}
-   </div>
-);
+class Ingredient extends PureComponent {
+   render() {
+      const { item } = this.props;
+
+      return (
+         <div className="recipe-ingredient">
+            {item.name}
+         </div>
+      );
+   }
+}
 
 Ingredient.propTypes = {
    item: PropTypes.objectOf(PropTypes.string),
